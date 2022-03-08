f9daf281f2c598deb4373d65b708535ba0fb5ed2
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-09 01:43:26 +0530

    Made Banner Responsive

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,9 +7,9 @@ function Banner() {
   return (
     <>
     
-    <div className = {`bg-[url(${Image})] h-[60vh] bg-center bg-cover flex items-end justify-center`}>
+    <div className = {`bg-[url(${Image})] h-[40vh] md:h-[60vh] bg-center bg-cover flex items-end justify-center`}>
 
-    	<div className = "text-3xl text-white p-4 bg-gray-900 w-full flex justify-center bg-opacity-50">Spider-Man : No Way Home</div>
+    	<div className = "text-xl md:text-3xl text-white p-4 bg-gray-900 w-full flex justify-center bg-opacity-50">Spider-Man : No Way Home</div>
     </div>
     
     </>
