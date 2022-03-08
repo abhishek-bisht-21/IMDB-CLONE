4d72af6797676c7bb23731cfd53689abd40705f8
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-09 02:28:51 +0530

    Routes Added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import NavBar from './components/NavBar'
 import Banner from './components/Banner';
 import Movies from './components/Movies';
 import Pagination from './components/Pagination';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Favourites from './components/Favourites';
 
 
 function App() {
   return (
 
-    <>
+    <BrowserRouter>
 
-    <NavBar></NavBar>   
-    <Banner></Banner>
-    <Movies></Movies>
-    <Pagination></Pagination>
+    <NavBar></NavBar> 
+
+    <Routes>
+            <Route path="/" element={  <>  <Banner/> <Movies/>  <Pagination/> </>} />
+            <Route path="/favourites" element={  <>  <Favourites/> </>} />
+
+    </Routes>
+
+      
+
+  
     
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from '../logo.png'
+import {Link} from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div className = "border pl-12 flex space-x-8 items-center py-4">
+  <div className = "border pl-12 flex space-x-8 items-center py-4">
+
 	<img className = "w-[50px] md:w-[60px]" src={logo}></img>
-	<div className = "text-blue-400 font-bold text-xl md:text-3xl">Movies</div>
-	<div className = "text-blue-400 font-bold text-xl md:text-3xl" > Favourites</div>
+	<Link to="/" className = "text-blue-400 font-bold text-xl md:text-3xl">Movies</Link>
+	<Link to="/favourites" className = "text-blue-400 font-bold text-xl md:text-3xl" > Favourites</Link>
     
     
     </div>
