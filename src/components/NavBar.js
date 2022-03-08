50bfcb739726947bca840c4d1d2f1d9dfcef30af
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-09 01:41:26 +0530

    Made Navbar Component Responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ import logo from '../logo.png'
 function NavBar() {
   return (
     <div className = "border pl-12 flex space-x-8 items-center py-4">
-	<img src={logo}></img>
-	<div className = "text-blue-400 font-bold text-3xl">Movies</div>
-	<div className = "text-blue-400 font-bold text-3xl" > Favourites</div>
+	<img className = "w-[50px] md:w-[60px]" src={logo}></img>
+	<div className = "text-blue-400 font-bold text-xl md:text-3xl">Movies</div>
+	<div className = "text-blue-400 font-bold text-xl md:text-3xl" > Favourites</div>
     
     
     </div>
