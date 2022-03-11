6250de75fb00878ae1aabd152bd873e5cfcf6d06
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-11 23:52:17 +0530

    Fav Page

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Pagination from './Pagination'
 
 function Favourites() {
+
+  const[curGenre,setCurGenre] = useState("All Genre");
+
   return (
     <>
 
-      <div className = "flex justify-center space-x-2 flex-wrap mt-4 px-2">
-            <button className = " m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400 " >All Genres</button>
-            <button className = " m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400" >Action</button>
-            <button className = " m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400" >Comedy</button>
-            <button className = " m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400" >Drama</button>
-            <button className = " m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400" >Animation</button>
+
+      <div className="flex justify-center space-x-2 flex-wrap mt-4 px-2">
+        <button className={
+          curGenre == "All Genres" ? "m-2 text-lg px-2 text-white p-1 rounded-xl bg-blue-400 font-bold " :
+            "m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400 "
+        }
+        >
+
+          All Genres
+
+        </button>
 
 
+        <button className = {
+          curGenre == "All Genres" ? "m-2 text-lg px-2 text-white p-1 rounded-xl bg-blue-400 font-bold " :
+            "m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400 "
+        } >Action</button>
+        {/* <button className=" m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400" >Comedy</button>
+        <button className=" m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400" >Drama</button>
+        <button className=" m-2 text-lg px-2 text-white p-1 rounded-xl bg-gray-400 font-bold  hover:bg-blue-400" >Animation</button> */}
+      </div>
+
+      <div className="text-center" >
+        <input type="text" placeholder="Search" className="border border-4 text-center p-1 m-2" />
+        <input type="number" placeholder="Rows" className="border border-4 text-center p-1 m-2" />
 
       </div>
+
+
       <div>Inputs Container</div>
       <div>Table Container</div>
-      <div>Pagination</div>
+
+      <div> <Pagination /> </div>
+      
 
     </>
   )
