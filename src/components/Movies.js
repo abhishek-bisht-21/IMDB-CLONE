f1a6932c37e74343e1aa7c06d0edb82565ca0096
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-11 11:31:18 +0530

    Displaying Movies using TMDB API

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Image from '../banner.jpg'
+import axios from 'axios'
+import {Oval} from 'react-loader-spinner'
+
+
+
 function Movies() {
+
+    const [movies, setMovies] = useState([])
+    useEffect(function(){
+
+    axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=23c43a4e9520652b54a64f91a9287dc6")
+    .then((res) => {
+        // console.table(res.data.results)
+        setMovies(res.data.results);
+    })
+
+    })
+
     return <>
-        <div className="mb-8">
+        <div className="mb-8 text-center">
             <div className="mt-8 mb-8 font-bold text-2xl text-center">Trending Movies</div>
+            {
+                movies.length == 0 ?
+                <div className = "flex justify-center"> 
+                <Oval
+                    height = "100"
+                    width = "100"
+                    color = "grey"
+                    secondaryColor = "grey"
+                    ariaLabel = "loading"
+
+                /> </div> : 
+
+            
+
             <div className="flex flex-wrap justify-center">
+                {
+                    movies.map((movie) => (
+
+                    
                 <div className={`
-                bg-[url(${Image})] 
+                bg-[url("https://image.tmdb.org/t/p/w500/${movie.backdrop_path}")] 
                 md:h-[30vh] md:w-[250px] 
                 h-[25vh] w-[150px]
                 bg-center bg-cover
@@ -16,115 +51,14 @@ function Movies() {
                 hover:scale-110
                 ease-out duration-300
             `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
-                </div>
-                <div className={`
-                bg-[url(${Image})] 
-                md:h-[30vh] md:w-[250px] 
-                h-[25vh] w-[150px]
-                bg-center bg-cover
-                rounded-xl
-                flex items-end
-                m-4
-                hover:scale-110
-                ease-out duration-300
-            `}>
-                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">Title </div>
+                    <div className="w-full bg-gray-900 text-white py-2 font-bold text-center rounded-b-xl">{movie.title} </div>
                 </div>
                 
-            </div>
+                    ))
+                }       
 
+            </div>
+             }   
         </div>
     </>
 }
